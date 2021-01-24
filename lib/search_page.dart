7c5063b1a42167933a348d152962e01c5fbeaf14
Juicy99 <karekareta@gmail.com>
2021-01-24 13:13:56 +0900

    検索機能　許可不足　メモ用

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -6,14 +6,13 @@ import 'package:sateiv2_app/order_notify.dart';
 import 'auth_service.dart';
 import 'order_notify.dart';
 
-class SearchPage extends StatefulWidget {
+class SearchPage1 extends StatefulWidget {
   @override
-  _SearchPageState createState() => _SearchPageState();
+  _SearchPageState1 createState() => _SearchPageState1();
 }
 
-class _SearchPageState extends State<SearchPage> {
+class _SearchPageState1 extends State<SearchPage1> {
   String name = "";
-  String uid;
   @override
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthService>(context);
@@ -32,9 +31,6 @@ class _SearchPageState extends State<SearchPage> {
           case ConnectionState.waiting: // データの取得まち
             return CircularProgressIndicator();
           default:
-            // streamからデータを取得できたので、使いやすい形にかえてあげる
-            historyService.init2(snapshot.data.docs);
-
             return Scaffold(
               appBar: AppBar(
                 leading: IconButton(
@@ -57,23 +53,21 @@ class _SearchPageState extends State<SearchPage> {
               ),
               body: StreamBuilder<QuerySnapshot>(
                 stream: (name != "" && name != null)
-                    ? FirebaseFirestore.instance
-                        .collection('users/$uid/itemHistory')
-                        .where("searchKeywords", arrayContains: name)
+                    ? historyService.dataPath1
+                        .where("name", isEqualTo: name)
                         .snapshots()
-                    : FirebaseFirestore.instance
-                        .collection('users/$uid/itemHistory')
-                        .snapshots(),
+                    : historyService.dataPath1.snapshots(),
                 builder: (context, snapshot) {
                   return (snapshot.connectionState == ConnectionState.waiting)
                       ? Center(child: CircularProgressIndicator())
                       : ListView.builder(
-                          itemCount: historyService.itemHistory.length,
+                          itemCount: snapshot.data.docs.length,
                           itemBuilder: (BuildContext context, int index) {
+                            DocumentSnapshot data = snapshot.data.docs[index];
                             return Card(
                               child: Row(
                                 children: <Widget>[
-                                  Text(historyService.itemHistory[index].name),
+                                  Text(data['name']),
                                 ],
                               ),
                             );
@@ -84,6 +78,151 @@ class _SearchPageState extends State<SearchPage> {
             );
         }
       },
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController controller;
+  bool isCaseSensitive = false;
+
+
+
+  final List<String> searchTargets =
+      List.generate(10, (index) => 'Something ${index + 1}');
+
+  List<String> searchResults = [];
+
+  void search(String query, {bool isCaseSensitive = false}) {
+    if (query.isEmpty) {
+      setState(() {
+        searchResults.clear();
+      });
+      return;
+    }
+
+    final List<String> hitItems = searchTargets.where((element) {
+      if (isCaseSensitive) {
+        return element.contains(query);
+      }
+      return element.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
+    setState(() {
+      searchResults = hitItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Items'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: const Text('Case Sensitive'),
+              value: isCaseSensitive,
+              onChanged: (bool newVal) {
+                setState(() {
+                  isCaseSensitive = newVal;
+                });
+                search(controller.text, isCaseSensitive: newVal);
+              },
+            ),
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(hintText: 'Enter keyword'),
+              onChanged: (String val) {
+                search(val, isCaseSensitive: isCaseSensitive);
+              },
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: searchResults.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: HighlightedText(
+                    wholeString: searchResults[index],
+                    highlightedString: controller.text,
+                    isCaseSensitive: isCaseSensitive,
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HighlightedText extends StatelessWidget {
+  HighlightedText({
+    @required this.wholeString,
+    @required this.highlightedString,
+    this.defaultStyle = const TextStyle(color: Colors.black),
+    this.highlightStyle = const TextStyle(color: Colors.blue),
+    this.isCaseSensitive = false,
+  });
+
+  final String wholeString;
+  final String highlightedString;
+  final TextStyle defaultStyle;
+  final TextStyle highlightStyle;
+  final bool isCaseSensitive;
+
+  int get _highlightStart {
+    if (isCaseSensitive) {
+      return wholeString.indexOf(highlightedString);
+    }
+    return wholeString.toLowerCase().indexOf(highlightedString.toLowerCase());
+  }
+
+  int get _highlightEnd => _highlightStart + highlightedString.length;
+
+  @override
+  Widget build(BuildContext context) {
+    // indexOf()は該当する要素が見つからない場合「-1」を返す。
+    // 検索キーワードを含んでいないので、ハイライトされていない素のテキストを表示。
+    if (_highlightStart == -1) {
+      return Text(wholeString, style: defaultStyle);
+    }
+    return RichText(
+      text: TextSpan(
+        style: defaultStyle,
+        children: [
+          TextSpan(text: wholeString.substring(0, _highlightStart)),
+          TextSpan(
+            text: wholeString.substring(_highlightStart, _highlightEnd),
+            style: highlightStyle,
+          ),
+          TextSpan(text: wholeString.substring(_highlightEnd))
+        ],
+      ),
     );
   }
 }
